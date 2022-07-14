5e0c191bd941ef9e2567fd84667c6a3b3ef44aef
Meka Seba <mekasebameka@gmail.com>
2022-07-14 16:23:51 -0700

    list items

--- guts.js
+++ guts.js
@@ -9,5 +9,6 @@ inputBtn.addEventListener("click", function() {
 }) 
 
 for (let i = 0; i < myLeads.length; i++) {
-    console.log("- " + myLeads[i])
+    console.log(myLeads[i])
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" + " "
 }

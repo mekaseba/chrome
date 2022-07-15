916d47522c3437795d69b147e52e537f5107fd34
Meka Seba <mekasebameka@gmail.com>
2022-07-14 18:13:25 -0700

    progress

--- guts.js
+++ guts.js
@@ -1,14 +1,18 @@
-let myLeads = ["i", "am", "dumb"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 }) 
 
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" + " "
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i])
+        listItems += "<li>" + myLeads[i] + "</li>" + " "
+    }
+    ulEl.innerHTML = listItems
 }

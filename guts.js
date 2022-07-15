292355f126f6e6c8d5c69cb8ac3097d891a699e5
Meka Seba <mekasebameka@gmail.com>
2022-07-15 08:10:15 -0700

    leads old and new

--- guts.js
+++ guts.js
@@ -1,26 +1,23 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-//localStorage.clear()
-console.log(leadsFromLocalStorage)
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-    renderLeads()
-    console.log(localStorage.getItem("myLeads"))
-}) 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='https://${myLeads[i]}'>
-                ${myLeads[i]}
+                <a target='_blank' href='https://${leads[i]}'>
+                ${leads[i]}
                 </a>
             </li>
         `
@@ -28,4 +25,16 @@ function renderLeads() {
     ulEl.innerHTML = listItems
 }
 
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+    render(myLeads)
+}) 
 

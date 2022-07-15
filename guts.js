b53e236c38a434e97f2f5849821cda65652082e3
Meka Seba <mekasebameka@gmail.com>
2022-07-15 07:11:39 -0700

    local storage functions

--- guts.js
+++ guts.js
@@ -2,11 +2,16 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+//localStorage.clear()
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
     renderLeads()
+    console.log(localStorage.getItem("myLeads"))
 }) 
 
 function renderLeads() {
@@ -22,3 +27,5 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+
